831f8c9b2f636c9875caefa5d7c7c1ece9e55ffc
Kamol <kamolkhamidov200002@gmail.com>
2023-01-15 21:57:46 +0500

    initial commit
    configured eslint, prettier, tsconfig, global styles and folders structure
    added necessary libs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  // config svg files
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(ts)x?$/] },
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: { plugins: [{ removeViewBox: false }] },
+          },
+        },
+      ],
+    })
+
+    return config
+  }
 }
 
 module.exports = nextConfig
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
+      <link
+        href='https://api.fontshare.com/v2/css?f[]=clash-display@400,700,500,600&display=swap'
+        rel='stylesheet'
+      />
       <body>
         <Main />
         <NextScript />
