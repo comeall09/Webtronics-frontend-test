dce920403fe99358d02cfb3df652cfb8d914f481
Kamol <kamolkhamidov200002@gmail.com>
2023-01-21 00:12:42 +0500

    correct cards gradients

--- src/widgets/steps/separator/index.tsx
+++ src/widgets/steps/separator/index.tsx
@@ -2,25 +2,24 @@ import Image from "next/image"
 import styles from "./styles.module.scss"
 
 const img = {
-  width: 95,
-  height: 25,
   src: "/assets/steps/arrow.svg",
   alt: "steps separator",
 }
 // array of 5 images
-const imgList = new Array(5).fill(img)
+const imgList = new Array(6).fill(img)
 
 export function Separator() {
   return (
     <div className={styles.wrapper}>
       <span className={styles.vertical}></span>
       <div className={styles.images}>
-        {imgList.map(({ width, height, src, alt }, index) => (
+        {imgList.map(({ src, alt }, index) => (
           <Image
+            priority
             key={index}
             src={src}
-            width={width}
-            height={height}
+            width={95}
+            height={25}
             alt={alt}
           />
         ))}
